4d5a8833dc7497247253cf38d65e73be0ac802f5
Szernex <szernex@gmail.com>
2014-07-20 23:36:00 +0200

    * added argument parsing via JOpt
    - omitting tests for now since stuff is working as intended and laziness is too stronk

--- src/main/java/org/szernex/folderflattener/Main.java
+++ src/main/java/org/szernex/folderflattener/Main.java
@@ -1,9 +1,13 @@
 package org.szernex.folderflattener;
 
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,14 +17,60 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		FileCollector collector = new FileCollector();
-		Set<File> input = collector.collect(new File("d://testing"));
-		Map<File, File> output = collector.flatten(new File("d://testing"), input);
+		OptionParser parser = new OptionParser();
+
+		parser.accepts("s", "The source root directory.")
+				.withRequiredArg()
+				.describedAs("source")
+				.required();
+		parser.accepts("t", "The target directoy. Can be identical to source.")
+				.withRequiredArg()
+				.describedAs("target")
+				.required();
+		parser.accepts("f", "The file filter in form of a regular expression. Optional.")
+				.withRequiredArg()
+				.describedAs("filter")
+				.defaultsTo(".*");
+
+		OptionSet options = null;
+
+		try
+		{
+			options = parser.parse(args);
+		}
+		catch (OptionException oex)
+		{
+			try
+			{
+				parser.printHelpOn(System.out);
+			}
+			catch (IOException ioex)
+			{
+				ioex.printStackTrace();
+			}
+		}
+
+
+		FileCollector collector = new FileCollector(options.valueOf("f").toString());
+		Set<File> input = collector.collect(new File(options.valueOf("s").toString()));
+		Map<File, File> output = collector.flatten(new File(options.valueOf("t").toString()), input);
+		int moved = 0;
 
 		for (Map.Entry<File, File> entry : output.entrySet())
 		{
 			System.out.print(entry.getKey() + " -> " + entry.getValue() + " : ");
-			System.out.println(entry.getKey().renameTo(entry.getValue()));
+
+			if (entry.getKey().renameTo(entry.getValue()))
+			{
+				System.out.println("OK");
+				moved++;
+			}
+			else
+			{
+				System.out.println("FAILED");
+			}
 		}
+
+		System.out.println("Finished. Moved " + moved + " file(s).");
 	}
 }
